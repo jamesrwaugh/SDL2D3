201785b570602006c4191d822e9c6b3f2950df4f
TehPwns <p00n3dj002@yahoo.com>
2015-10-17 11:48:39 -0400

    Update to new version of LTBL2, fixed constructor warning

--- ltbl/quadtree/QuadtreeOccupant.cpp
+++ ltbl/quadtree/QuadtreeOccupant.cpp
@@ -2,16 +2,25 @@
 
 #include <ltbl/quadtree/QuadtreeNode.h>
 
+#include <ltbl/quadtree/Quadtree.h>
+
 #include <assert.h>
 
 using namespace ltbl;
 
 void QuadtreeOccupant::quadtreeUpdate() {
-	if (_pQuadtreeNode != nullptr)
-		_pQuadtreeNode->update(this);
+    if (_pQuadtreeNode != nullptr)
+        _pQuadtreeNode->update(this);
+    else {
+        _pQuadtree->_outsideRoot.erase(this);
+
+        _pQuadtree->add(this);
+    }
 }
 
 void QuadtreeOccupant::quadtreeRemove() {
-	if (_pQuadtreeNode != nullptr)
-		_pQuadtreeNode->remove(this);
+    if (_pQuadtreeNode != nullptr)
+        _pQuadtreeNode->remove(this);
+    else
+        _pQuadtree->_outsideRoot.erase(this);
 }
